69a25ff14dcaf55ed25db614a30e1dee6527f283
Ryan Cook <ryan@quickleft.com>
2010-06-25 19:44:08 -0600

    initial commit - v1 of file

---
+++ boulder_entrepreneur_community.mm
@@ -0,0 +1,35 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1273619233793" ID="0s168k96s5qqrdkambilevi6vf" MODIFIED="1273619234172" TEXT="Boulder Entrepreneur Community">
+<node CREATED="1273619233793" ID="1jrhtn980sup2t7t1d1vgnrgpa" MODIFIED="1273619234174" POSITION="right" TEXT="Forums/Events">
+<node CREATED="1273619233793" ID="1l03h156tb85klsuvlmg2aig60" LINK="http://www.bdnewtech.com/" MODIFIED="1273619234174" TEXT="New Tech Meetup"/>
+<node CREATED="1273619233793" ID="4fjgjdlvekcempqijr0vikqfec" LINK="http://www.facebook.com/BoulderOpenCoffeeClub" MODIFIED="1273619234175" TEXT="Boulder Open Coffee Club"/>
+<node CREATED="1273619233794" ID="54pihg8qu03ci8pc4dedg8ktjk" LINK="http://boulder.me/calendar/" MODIFIED="1273619234175" TEXT="Dev Geeks"/>
+<node CREATED="1273619233794" ID="6g7tp2h8p79gj05p9it4chb68l" LINK="http://www.meetup.com/startupwomen/" MODIFIED="1273619234175" TEXT="Startup Women"/>
+<node CREATED="1273619233794" ID="0ded4lalpe6bpsrqsaht1lva9q" LINK="http://www.girlsintech303.com/" MODIFIED="1273619234175" TEXT="Girls in Tech"/>
+<node CREATED="1273619233794" ID="0jk3anrt65acm7tvr8669ra7mf" LINK="http://boulder.startupweekend.org/" MODIFIED="1273620109861" TEXT="Startup Weekend"/>
+<node CREATED="1273619233794" ID="5lnjrl5ra5n9vdftird9qhcsp6" LINK="http://boco.me/" MODIFIED="1273619234175" TEXT="Boco"/>
+<node CREATED="1273619233794" ID="1raciv2umu5350agh5o0j6g65v" LINK="http://boulderstartupweek.com/" MODIFIED="1273619234175" TEXT="Startup Week"/>
+<node CREATED="1273619233794" ID="6ah788iag4buuin8pm9ubvd84s" LINK="http://igniteboulder.com/" MODIFIED="1273619234175" TEXT="Ignite"/>
+</node>
+<node CREATED="1273619233794" ID="2iqfoeqj1crk9svvl070s63r37" MODIFIED="1273619234175" POSITION="right" TEXT="Education">
+<node CREATED="1273619233794" ID="0n0e320186fnd35k5l12hls3rb" LINK="http://www.bouldersbdc.com/" MODIFIED="1273619234175" TEXT="Small Business Development Center"/>
+<node CREATED="1273619233794" ID="1k1oplnhhhmop0d528det0u9vi" MODIFIED="1273619234175" TEXT="CU">
+<node CREATED="1273619233794" ID="08sspfhm6eusg4pse09bl9vqiu" LINK="http://leeds.colorado.edu/Deming/interior.aspx?id=548" MODIFIED="1273619234175" TEXT="Deming Center for Entrepreneurship"/>
+<node CREATED="1273619233794" ID="3o7gd2u4km37iar3jf7jqtd30b" LINK="http://www.colorado.edu/law/clinics/entre/" MODIFIED="1273619234175" TEXT="Entrepreneurial Law Clinic"/>
+</node>
+</node>
+<node CREATED="1273619233794" ID="2esk4mglt4kgr9b439uvbrbsbt" MODIFIED="1273619234175" POSITION="left" TEXT="Press">
+<node CREATED="1273619233794" ID="2pin145n5nf00321300of4b9sf" LINK="http://www.fastcompany.com/article/why-you-should-start-company-boulder" MODIFIED="1273619234175" TEXT="Apr 2010 Fast Company"/>
+<node CREATED="1273619233794" ID="10m9sfuev0tkr167ql1b6gdcr7" LINK="http://www.businessweek.com/smallbiz/content/apr2010/sb20100421_531161.htm" MODIFIED="1273619234175" TEXT="Apr 2010 Business Week"/>
+</node>
+<node CREATED="1273619233793" ID="3h29f797mihdnhtq8ta35emnfe" MODIFIED="1273619234172" POSITION="left" TEXT="Funders/Accelerators">
+<node CREATED="1273619233793" ID="2d3jcrd9fl3rq3ipkguhbk1kph" LINK="http://www.foundrygroup.com/" MODIFIED="1273619234172" TEXT="Foundry Group"/>
+<node CREATED="1273619233793" ID="6seef90lutsobh2ngl51d20tg0" LINK="http://www.boulderventures.com/" MODIFIED="1273619234174" TEXT="Boulder Ventures"/>
+<node CREATED="1273619233793" ID="6aotcki3rlq55dk6qd4urhgd52" LINK="http://www.vistavc.com/" MODIFIED="1273619234174" TEXT="Vista Ventures"/>
+<node CREATED="1273619233793" ID="12rlkolch7g34fj98krvkm3c2o" LINK="http://www.sequelvc.com/" MODIFIED="1273619234174" TEXT="Sequel Venture Partners"/>
+<node CREATED="1273619233793" ID="5pp23udrhuno796umqlpnkbof6" LINK="http://www.techstars.org/" MODIFIED="1273619234174" TEXT="TechStars"/>
+<node CREATED="1273619233793" ID="431ooh7r828cvtt32ehl2tq0o1" LINK="http://www.boulderinnovationcenter.com/" MODIFIED="1273619234174" TEXT="Boulder Innovation Center"/>
+</node>
+</node>
+</map>
